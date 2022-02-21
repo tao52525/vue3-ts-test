54997df09b5ef2e437836f1c0dc0f6ae4380b68a
Jin Tao <jerome891585360@gmail.com>
2022-02-22 02:18:42 +0800

    feat

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
-const 
+const Home = () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+const About = () => import(/* webpackChunkName: "About" */ '@/views/About.vue')
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
-  ]
+]
+export default routes
